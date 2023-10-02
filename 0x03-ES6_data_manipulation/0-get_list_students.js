98516e778b4a73aa794ef91750e0106505f8ff9e
mainangaruiya <003paulmaina@gmail.com>
2023-10-02 12:28:45 +0300

    function getliststudent for array of object

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
-function getListStudents(){
+function getListStudents = () => {
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
-  return students;
 }
+
 export default getListStudents;
