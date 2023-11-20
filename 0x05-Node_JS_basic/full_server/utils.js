2a6138de3e9e8f3022b9c2fdb58038f2b19a9d2f
mainangaruiya <003paulmaina@gmail.com>
2023-11-20 16:22:41 +0300

    server

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -4,28 +4,62 @@ const fs = require('fs/promises');
 **/
 
 const readDatabase = (dataPath) => new Promise((resolve, reject) => {
-  fs.readFile(dataPath, 'utf8')
-    .then((data) => {
-      // Process the data and generate the object of arrays
-      const lines = data.split('\n');
-      const students = [];
-      const fieldCounts = {};
-
-      for (const line of lines) {
-        if (line === '') continue;
-        const [firstName, field] = line.split(',');
-        students.push({ firstName, field });
-
-        if (!fieldCounts[field]) {
-          fieldCounts[field] = [];
-        }
-        fieldCounts[field].push(student.firstName);
+  // Check if dataPath is not provided
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  // Check if dataPath is provided
+  if (dataPath) {
+    // Read the contents of the file asynchronously
+    fs.readFile(dataPath, (err, data) => {
+      // Handle errors during file reading
+      if (err) {
+        reject(new Error('Cannot load the database'));
       }
+      // Check if data is available
+      if (data) {
+        // Split the file content into lines
+        const fileLines = data
+          .toString('utf-8')
+          .trim()
+          .split('\n');
+        // Initialize an object to store student groups
+        const studentGroups = {};
+        // Extract field names from the first line of the file
+        const dbFieldNames = fileLines[0].split(',');
+        // Extract property names (excluding the last field)
+        const studentPropNames = dbFieldNames
+          .slice(0, dbFieldNames.length - 1);
 
-      resolve(fieldCounts);
-    })
-    .catch((error) => {
-      // Handle any errors and reject the Promise
-      reject(error);
+        // Iterate through each line (excluding the first line)
+        for (const line of fileLines.slice(1)) {
+          // Split the line into individual values
+          const studentRecord = line.split(',');
+          // Extract property values (excluding the last field)
+          const studentPropValues = studentRecord
+            .slice(0, studentRecord.length - 1);
+          // Extract the field name (last value in the line)
+          const field = studentRecord[studentRecord.length - 1];
+
+          // Initialize an array for the current field if not present
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
+          }
+
+          // Create an array of key-value pairs for each student
+          const studentEntries = studentPropNames
+            .map((propName, idx) => [propName, studentPropValues[idx]]);
+
+          // Convert the array of key-value pairs to an object and push it to the corresponding field
+          studentGroups[field].push(Object.fromEntries(studentEntries));
+        }
+
+        // Resolve the promise with the studentGroups object
+        resolve(studentGroups);
+      }
     });
+  }
 });
+
+// Export the readDatabase function as the default export of the module
+export default readDatabase;
