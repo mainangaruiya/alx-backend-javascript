9244091143c2d596c7cfdbde6e1b0b67df089426
mainangaruiya <003paulmaina@gmail.com>
2023-11-22 11:35:24 +0300

    function named calculateNumber. It should accepts two arguments (number) a and b The function should round a and b and return the sum of it

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+/*
+*round tonearest int then get sum 
+*/
+
+function calaculateNumber(a, b){
+	const an = Math.round(a);
+	const bn = Math.round(b);
+	const c = an + bn ;
+
+	return c;
+} 
+module.exports = calaculateNumber;
