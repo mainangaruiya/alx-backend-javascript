1890b1985b222c1cf5d19d00ae8f314031c1499d
Maina Ngaruiya <100405059+mainangaruiya@users.noreply.github.com>
2023-11-22 09:56:09 +0300

    Install Express and in a file named 6-http_express.js, create a small HTTP server using Express module

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-	res.send('Hello Holberton School!');
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
 });
 
 module.exports = app;
