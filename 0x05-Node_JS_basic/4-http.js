3a799c8f33a542b1553b0e9fbe128ef1122ba39e
Maina Ngaruiya <100405059+mainangaruiya@users.noreply.github.com>
2023-11-22 09:53:18 +0300

    In a file named 4-http.js, create a small HTTP server using the http module:  It should be assigned to the variable app and this one must be exported HTTP server should listen on port 1245 Displays Hello Holberton School! in the page body for any endpoint as plain text

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Holberton School!');
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server listening on port 1245');
+app.listen(port, hostname, () => {
 });
 
 module.exports = app;
