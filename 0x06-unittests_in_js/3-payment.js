0a65afe516e31e1d6b9c8c0c9118f69e971a8911
mainangaruiya <003paulmaina@gmail.com>
2023-11-22 15:21:22 +0300

    Spies are a useful wrapper that will execute the wrapped function, and log useful information (e.g. was it called, with what arguments). Sinon is a library allowing you to create spies.

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,10 @@
+const Utils = require("./utils");
+/**
+ *payment request to api
+ * 
+ */
+function sendPaymentRequestToApi(totalAmount, totalShipping){
+	const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+	console.log('The total is: ${result}');
+}
+module.exports = sendPaymentRequestToApi;
