9cd70cf95e3743c6578c6c5cc826d908eea6f3ef
mainangaruiya <003paulmaina@gmail.com>
2023-11-22 15:35:27 +0300

    Stubs are similar to spies. Except that you can provide a different implementation of the function you are wrapping. Sinon can be used as well for stubs.

--- 0x06-unittests_in_js/4-payment.js
+++ 0x06-unittests_in_js/4-payment.js
@@ -1,10 +1,14 @@
 const Utils = require("./utils");
+
 /**
- *payment request to api
+ * Payment request to the API
  * 
+ * @param {number} totalAmount - The total amount of the payment.
+ * @param {number} totalShipping - The total shipping cost.
  */
-function sendPaymentRequestToApi(totalAmount, totalShipping){
-	const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
-	console.log('The total is: ${result}');
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
 }
+
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,16 +1,20 @@
-const {describe, it} = require("mocha");
+const { describe, it } = require("mocha");
 const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./3-payment.js");
 const Utils = require("./utils");
 const assert = require("assert");
 
-describe("sendPaymentRequestToApi", function() {
-    it("check that Utils.calculateNumber is stubbed", function() {
-	const spy = sinon.spy(console, "log");
-	const stub = sinon.stub(Utils, "calculateNumber").returns(10);
-	sendPaymentRequestToApi(100, 20);
+describe("sendPaymentRequestToApi", function () {
+  it("check that Utils.calculateNumber is stubbed", function () {
+    const spy = sinon.spy(console, "log");
+    const stub = sinon.stub(Utils, "calculateNumber").returns(30);
 
-	assert(spy.withArgs("The total is: 10").calledOnce);
-	assert(stub.withArgs("SUM", 100, 20).calledOnce);
-    });
+    sendPaymentRequestToApi(100, 20);
+
+    // Ensure that console.log was called with the expected message
+    assert(spy.calledOnceWithExactly("The total is: 30"));
+
+    // Restore the stub after the test
+    stub.restore();
+  });
 });
