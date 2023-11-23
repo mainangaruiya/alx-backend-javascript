00d18723e15d3fb3c549e10b56156154fc54b164
mainangaruiya <003paulmaina@gmail.com>
2023-11-23 11:00:42 +0300

    fixing typos

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,24 +1,24 @@
-
 /*
-*round tonearest int then get sum 
-*/
+ * Round to the nearest int then get sum
+ */
 
-function calaculateNumber (type, a, b) {
-	const an = Math.round(a);
-	const bn = Math.round(b);
-	
-	switch(type) {
-		case 'SUM':
-			return an + bn;
-		case 'SUBTRACT':
-			return an - bn;
-		case 'DIVIDE':
-			if (bn === 0){
-				return'Error'
-			}
-			return an/bn;
-		default:
-			throw new Error('Invalid operation type');
-	}
-} 
-module.exports = calaculateNumber;
+function calculateNumber(type, a, b) {
+  const an = Math.round(a);
+  const bn = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return an + bn;
+    case 'SUBTRACT':
+      return an - bn;
+    case 'DIVIDE':
+      if (bn === 0) {
+        return 'Error';
+      }
+      return an / bn;
+    default:
+      throw new Error('Invalid operation type');
+  }
+}
+
+module.exports = calculateNumber;
