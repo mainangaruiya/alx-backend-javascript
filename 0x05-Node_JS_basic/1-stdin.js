5274bc0ad7dd750ddeec215e44793e0fda1f678a
mainangaruiya <003paulmaina@gmail.com>
2023-11-24 12:50:44 +0300

    program named 1-stdin.js that will be executed through command line: fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,12 +8,17 @@ This important software is now closing (followed by a new line)
 **/
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim(); // Trim to remove any leading/trailing whitespace
-  if (input.toLowerCase() === 'exit') {
-    process.stdout.write('This important software is now closing\n');
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-  } else {
-    process.stdout.write(`Your name is: ${input}`);
-  }
-});
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
